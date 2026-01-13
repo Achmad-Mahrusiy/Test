ed27c584286041beae0e44aaa05e0cbd4257f754
troii <lolitunbina@gmail.com>
2026-01-13 19:58:59 +0700

    nama dan data kelahiran

--- C-Sharp/Nama.cs
+++ C-Sharp/Nama.cs
@@ -13,9 +13,21 @@ namespace MyFirstProgram
             String lastName = Console.ReadLine();
 
             String fullName = firstName + " " + lastName;
-            String userName = fullName.Insert(0, "Mr. ");
+            String userName = fullName.Insert(0, "Tuan atau nyonya. ");
+
+            Console.WriteLine("Masukkan Tahun kelahiran anda");
+            String tahun = Console.ReadLine();
+
+            Console.WriteLine("Masukkan Bulan kelahiran anda");
+            String bulan = Console.ReadLine();
+
+            Console.WriteLine("Masukkan Hari kelahiran anda");
+            String hari = Console.ReadLine();
 
             Console.WriteLine("Selamat Datang " + userName);
+
+            Console.WriteLine("Data kelahiran anda pada tahun " + tahun + " bulan "+ bulan + " tanggal "+ hari );
+
             Console.ReadKey();
         }
     }

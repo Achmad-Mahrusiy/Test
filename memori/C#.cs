4160ff098ae921613c3d67494634d3553550a436
troii <lolitunbina@gmail.com>
2026-01-13 09:41:20 +0700

    percobaan

---
+++ memori/C#.cs
@@ -0,0 +1,13 @@
+using System;
+
+  namespace MyFirstProgram
+  {
+
+    class Program
+    {
+      static void Main(string[] args)
+      {
+        console.WriteLine("Hidup Jokowi");
+      }
+    } 
+  }
